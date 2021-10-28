c99ab1774b13673b1db2b12999f11285741095e5
Jonas Johan Solsvik <jonasjso@protonmail.com>
2021-10-28 14:34:00 +0200

    add 4 activities
    - PeopleActivity
    - PlanetsActivity
    - FilmsActivity
    - SpaceshipsActivit

--- android/app/src/main/java/com/example/gruppearbeid/PlanetsActivity.kt
+++ android/app/src/main/java/com/example/gruppearbeid/PlanetsActivity.kt
@@ -3,9 +3,9 @@ package com.example.gruppearbeid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PlanetsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_planets)
     }
 }
---
+++ android/app/src/main/java/com/example/gruppearbeid/SpaceshipsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gruppearbeid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpaceshipsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spaceships)
+    }
+}
