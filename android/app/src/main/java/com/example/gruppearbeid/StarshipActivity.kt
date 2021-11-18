a968f500cdae73b57886bdd3835977705a807072
Jorgen Johnsen <jamjohns@stud.ntnu.no>
2021-11-18 16:41:42 +0100

    merge and resolve conflicts

---
+++ android/app/src/main/java/com/example/gruppearbeid/StarshipActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gruppearbeid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+// Class for a single starship
+class StarshipActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_starship)
+    }
+}
--- android/app/src/main/java/com/example/gruppearbeid/types/Starship.kt
+++ android/app/src/main/java/com/example/gruppearbeid/types/Starship.kt
@@ -3,4 +3,8 @@ package com.example.gruppearbeid.types
 data class Starship(
     /** "name": "Death Star" */
     val name: String,
-)
+    /** "model": "DS-1 Orbital Battle Station" **/
+    val model: String,
+
+    val manufacturer: String,
+    )
