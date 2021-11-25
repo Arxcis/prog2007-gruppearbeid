9a5c4773b2736415882a3de36a69151a157ce9c1
jonas johan solsvik <jonasjso@protonmail.com>
2021-11-25 23:06:09 +0100

    add emojis to all 4 adapters as well

--- android/app/src/main/java/com/example/gruppearbeid/adapters/PlanetsAdapter.kt
+++ android/app/src/main/java/com/example/gruppearbeid/adapters/PlanetsAdapter.kt
@@ -35,7 +35,7 @@ class PlanetsAdapter(
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val planet = planets[position]
-        holder.name?.text = planet.name
+        holder.name?.text = "🪐 ${planet.name}"
         holder.item?.setOnClickListener { onClick(planet) }
     }
 
