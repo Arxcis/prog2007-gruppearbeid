71cb4d7cafc8be818cbf4b121d6955731d486de5
Bibli2311 <oscar.vagle@yahoo.no>
2021-11-28 18:36:22 +0100

    added editText tag to UI. with text which is visible in both dark mode and normal mode

--- android/app/src/main/java/com/example/gruppearbeid/ChooseImage.kt
+++ android/app/src/main/java/com/example/gruppearbeid/ChooseImage.kt
@@ -1,11 +1,15 @@
 package com.example.gruppearbeid
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gruppearbeid.databinding.ActivityChooseImageBinding
 import com.example.gruppearbeid.util.Constants
@@ -14,15 +18,32 @@ class ChooseImage : AppCompatActivity() {
     private lateinit var _binding: ActivityChooseImageBinding
 
     private lateinit var url: String
+    private lateinit var requestCode: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityChooseImageBinding.inflate(layoutInflater)
         setContentView(_binding.root)
 
+        requestCode = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean -> }
+
+
         url = intent.getStringExtra(Constants.API_DATA_ENTITY)!!
         url?.let {
             Log.d("chooseImage", "url: ${it}")
         }
+    }
+
+    fun checkPermission() : Boolean{
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+            PackageManager.PERMISSION_GRANTED)
+        {
+            return true
+        }
+        else {
+            requestCode.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            return true
+        }
+        return false
     }
 }
