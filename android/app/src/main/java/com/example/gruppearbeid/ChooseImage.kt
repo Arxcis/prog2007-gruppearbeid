090557bf1672521e7f3d4f1fc971afff71b2edf6
Bibli2311 <oscar.vagle@yahoo.no>
2021-11-28 11:33:45 +0100

    added UI for adding image. Simpler UI which also works with dark mode

--- android/app/src/main/java/com/example/gruppearbeid/ChooseImage.kt
+++ android/app/src/main/java/com/example/gruppearbeid/ChooseImage.kt
@@ -9,29 +9,13 @@ import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gruppearbeid.databinding.ActivityChooseImageBinding
 
-class ChooseImage : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class ChooseImage : AppCompatActivity() {
     private lateinit var _binding: ActivityChooseImageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityChooseImageBinding.inflate(layoutInflater)
         setContentView(_binding.root)
-
-        //choose adapter which holds data of different entries
-        val spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.choices_select_image,android.R.layout.simple_spinner_item)
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        _binding.spinnerListEntities.adapter = spinnerAdapter
-
-        _binding.spinnerListEntities.onItemSelectedListener = this        //link SpinnerActivity to the onSelectedListener
-                                                                                       //of spinner.
-    }
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        Log.d("ChooseImageActi", parent?.getItemAtPosition(position).toString())
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        TODO("Not yet implemented")
     }
 
     override fun onDestroy() {
